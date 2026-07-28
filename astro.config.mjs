5888977f6a2c7100314253b3fb93fc26a0609a78
cm <cm@email.com>
2026-07-28 15:39:57 -0700

    Fix: Add base path for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://crescentmoondesign.com',
+  base: '/Crescent-Moon-Design/',
   integrations: [mdx(), sitemap()],
 });
